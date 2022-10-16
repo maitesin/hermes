0884324018b3f57c6335743711e924aff1e53668
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2022-10-16 10:22:16 +0200

    Improved test coverage

--- pkg/tracker/correos/tracker_test.go
+++ pkg/tracker/correos/tracker_test.go
@@ -2,8 +2,11 @@ package correos_test
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
 	"io"
 	"net/http"
+	"net/url"
 	"testing"
 
 	"github.com/maitesin/hermes/pkg/tracker"
@@ -20,13 +23,19 @@ func (rtm *roundTripperMock) RoundTrip(*http.Request) (*http.Response, error) {
 	return rtm.Response, rtm.RespErr
 }
 
-func clientResponding(response string) *http.Client {
+func workingClient(response string) *http.Client {
 	return &http.Client{
 		Transport: &roundTripperMock{
 			Response: &http.Response{
 				Body: io.NopCloser(bytes.NewBufferString(response)),
 			},
 		},
+	}
+}
+
+func failingClient() *http.Client {
+	return &http.Client{
+		Transport: &roundTripperMock{},
 	}
 }
 
@@ -52,18 +61,55 @@ func validPayload() string {
 }`
 }
 
+func tooManyShipmentsPayload() string {
+	return `{
+  "shipment": [
+    {
+      "events": [
+        {
+          "eventDate": "18/11/2021",
+          "eventTime": "18:25:51",
+          "phase": "2",
+          "colour": "V",
+          "summaryText": "Admitido",
+          "extendedText": "El envío ha tenido admisión en origen",
+          "actionWeb": null,
+          "actionWebParam": null,
+          "codired": "1254002"
+        }
+      ]
+    },
+    {
+      "events": [
+        {
+          "eventDate": "18/11/2021",
+          "eventTime": "18:25:51",
+          "phase": "2",
+          "colour": "V",
+          "summaryText": "Admitido",
+          "extendedText": "El envío ha tenido admisión en origen",
+          "actionWeb": null,
+          "actionWebParam": null,
+          "codired": "1254002"
+        }
+      ]
+    }
+  ]
+}`
+}
+
 func TestTracker_Track(t *testing.T) {
 	tests := []struct {
-		name      string
-		client    *http.Client
-		trackerID string
-		want      []tracker.DeliveryEvent
-		wantErr   error
+		name    string
+		client  *http.Client
+		want    []tracker.DeliveryEvent
+		wantErr error
 	}{
 		{
-			name:      "",
-			client:    clientResponding(validPayload()),
-			trackerID: "",
+			name: `Given a working HTTP client and a valid tracker ID,
+                   when the method Track is called,
+                   then it returns a list of delivery events`,
+			client: workingClient(validPayload()),
 			want: []tracker.DeliveryEvent{
 				{
 					Timestamp:   "18/11/2021 18:25:51",
@@ -72,21 +118,46 @@ func TestTracker_Track(t *testing.T) {
 			},
 			wantErr: nil,
 		},
+		{
+			name: `Given a failing HTTP client and a valid tracker ID,
+                   when the method Track is called,
+                   then it returns an error regarding the HTTP client not working`,
+			client:  failingClient(),
+			want:    nil,
+			wantErr: &url.Error{},
+		},
+		{
+			name: `Given a working HTTP client and a valid tracker ID,
+                   when the method Track is called and the response is an invalid JSON,
+                   then it returns an error regarding the response body`,
+			client:  workingClient(""),
+			want:    nil,
+			wantErr: &json.SyntaxError{},
+		},
+		{
+			name: `Given a working HTTP client and a valid tracker ID,
+                   when the method Track is called and the response contains multiple shipment information,
+                   then it returns an error regarding the response containing multiple shipment information`,
+			client:  workingClient(tooManyShipmentsPayload()),
+			want:    nil,
+			wantErr: errors.New(""),
+		},
 	}
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
+
 			trackerTest, err := correos.NewTracker(tt.client)
 			require.Nil(t, err)
 
-			got, err := trackerTest.Track(tt.trackerID)
+			got, err := trackerTest.Track("does not matter regarding testing")
 			if tt.wantErr != nil {
-				require.ErrorIs(t, err, tt.wantErr)
-				return
+				require.ErrorAs(t, err, &tt.wantErr)
+			} else {
+				require.Nil(t, err)
 			}
 
-			require.Nil(t, err)
 			require.Equal(t, tt.want, got)
 		})
 	}
