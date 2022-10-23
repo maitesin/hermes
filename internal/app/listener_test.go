0fb953984b51d0f54c3f2499cd1cb6b9e2df2660
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2022-10-23 17:33:08 +0200

    Improve test coverage in the app.Listen function

--- internal/app/listener_test.go
+++ internal/app/listener_test.go
@@ -25,7 +25,9 @@ func TestListen(t *testing.T) {
 		wantErr                    error
 	}{
 		{
-			name: ``,
+			name: `Given a working tracker, a working deliveries repository and a valid tracking ID,
+					when the Listen function received a message containing the valid tracking ID,
+					then the information retrieved from the tracker gets stored in the deliveries repository`,
 			message: comm.Message{
 				Text: "1234",
 			},
@@ -36,6 +38,39 @@ func TestListen(t *testing.T) {
 				mdr.EXPECT().Insert(context.Background(), app.NewDelivery("1234", "[]", 0, false))
 			},
 			wantErr: nil,
+		},
+		{
+			name: `Given a failing tracker, a working deliveries repository and a valid tracking ID,
+					when the Listen function received a message containing the valid tracking ID,
+					then the tracker fails to obtain the information and returns an error`,
+			message: comm.Message{
+				Text: "1234",
+			},
+			trackerChecks: func(mt *tmock.MockTracker) {
+				mt.EXPECT().Track("1234").Return(nil, false, app.NewDeliveryNotFoundError("1234"))
+			},
+			deliveriesRepositoryChecks: func(mdr *appMock.MockDeliveriesRepository) {},
+			wantErr:                    app.NewDeliveryNotFoundError("1234"),
+		},
+		{
+			name: `Given a working tracker, a failing deliveries repository and a valid tracking ID,
+					when the Listen function received a message containing the valid tracking ID,
+					then the tracker obtains the information, but the deliveries repository fails to store it and returns an error`,
+			message: comm.Message{
+				Text: "1234",
+			},
+			trackerChecks: func(mt *tmock.MockTracker) {
+				mt.EXPECT().Track("1234")
+			},
+			deliveriesRepositoryChecks: func(mdr *appMock.MockDeliveriesRepository) {
+				mdr.EXPECT().
+					Insert(
+						context.Background(),
+						app.NewDelivery("1234", "[]", 0, false),
+					).
+					Return(app.NewDeliveryNotFoundError("1234"))
+			},
+			wantErr: app.NewDeliveryNotFoundError("1234"),
 		},
 	}
 	for _, tt := range tests {
